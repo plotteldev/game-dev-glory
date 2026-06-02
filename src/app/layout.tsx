f516561c8191e0498cb25e49c09e76c891950318
Matt Noone <plotteldev@gmail.com>
2026-06-03 09:21:46 +1000

    Add Google Ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager, GoogleTagManagerNoScript } from "@/components/google-tag-manager";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -48,7 +49,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <GoogleTagManagerNoScript />
+        {children}
+        <GoogleTagManager />
+      </body>
     </html>
   );
 }
---
+++ src/components/google-tag-manager.tsx
@@ -0,0 +1,52 @@
+import Script from "next/script";
+
+const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+const googleAdsId = "AW-18205791019";
+
+export function GoogleTagManager() {
+  return (
+    <>
+      {gtmId ? (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
+      ) : null}
+      <Script
+        id="google-ads-tag"
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleAdsId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads-config" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${googleAdsId}');
+        `}
+      </Script>
+    </>
+  );
+}
+
+export function GoogleTagManagerNoScript() {
+  if (!gtmId) {
+    return null;
+  }
+
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
+  );
+}
